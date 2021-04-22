6cf48b3aa54350b0be3c64fad6b6c2efdd8d7414
Aung Phone Khant <morriszin016@gmail.com>
2021-04-22 17:14:26 +0630

    Modified input field

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,7 +11,7 @@ const Search = ({onTextSubmit}) => {
     return (
         <div className="flex flex-col mt-6">   
             <label htmlFor="search-bar" className="text-center text-gray-darkest mt-12 text-lg font-bold uppercase mb-3 tracking-wide">Search for city ...</label>
-            <input id="search-bar" autoComplete="off"  className=" z-5 focus:border-gray-200 border border-gray-100 mt-4 apperance-none text-gray-darkest rounded-lg shadow-lg outline-none px-3 py-2 text-gray-500" type="text" value={text} onChange={onTextChange} />
+            <input id="search-bar" autoComplete="off"  className="text-center z-5 focus:border-gray-200 border border-gray-100 mt-4 apperance-none text-gray-darkest rounded-lg shadow-lg outline-none px-3 py-2 text-gray-500" type="text" value={text} onChange={onTextChange} />
         </div>  
     )
 }
