561f7e643803d1b1fce451177257c79aae38232e
Aung Phone Khant <morriszin016@gmail.com>
2021-05-03 15:18:53 +0630

    Added lazy loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import fetchWeatherApi from '../api/fetchWeather'
-import Description from './Description';
-import Header from './Header';
-import Search from './Search';
-const key = '55fe789aed680b0611ee4ed1432c0f7e';
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import fetchWeatherApi from "../api/fetchWeather";
+import Search from "./Search";
+
+const Description = lazy(() => import("./Description"));
+const Header = lazy(() => import("./Header"));
+
+const key = "55fe789aed680b0611ee4ed1432c0f7e";
 
 const App = () => {
+  const [search, setSearch] = useState("Mandalay");
+  const [debounceText, setDebounceText] = useState(search);
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(false);
 
-    const [search,setSearch] = useState('Mandalay')
-    const [debounceText, setDebounceText] = useState(search); 
-    const [result, setResult] = useState(null); 
-    const [error, setError] = useState(false); 
-    
-    useEffect(() => {
+  useEffect(() => {
+    const setTimeOutId = setTimeout(() => {
+      setError(false);
+      setDebounceText(search);
+    }, 500);
 
-        const setTimeOutId = setTimeout(() => {
-        setError(false); 
-           setDebounceText(search);
-        },500); 
+    return () => clearTimeout(setTimeOutId);
+  }, [search]);
 
-        return () => clearTimeout(setTimeOutId); 
+  useEffect(() => {
+    const fetchWeather = () => {
+      fetchWeatherApi
+        .get("data/2.5/weather", {
+          params: {
+            q: debounceText,
+            appid: key,
+          },
+        })
+        .then((result) => {
+          setResult(result.data);
+          console.clear();
+        })
+        .catch((e) => {
+          console.log("Error", e);
+          setError(true);
+        });
+    };
+    debounceText && fetchWeather();
+  }, [debounceText]);
+  const onTextSubmit = (q) => setSearch(q);
 
-    }, [search]); 
-
-    useEffect(() => {
-
-        const fetchWeather =  () => {
-           fetchWeatherApi.get('data/2.5/weather', {
-                params: {
-                    q: debounceText,
-                    appid: key
-                }
-            })
-            .then(result => {
-                setResult(result.data); 
-                console.clear();
-            })
-            .catch(e =>  {
-                console.log('Error', e); 
-                setError(true); 
-            })
-        }
-        debounceText && fetchWeather(); 
-        
-
-    },[debounceText])
-    const onTextSubmit = q => setSearch(q)
-
-    return (
-        <div className="flex justify-around w-screen h-screen flex-col">
-            <div className="mt-4 flex justify-center w-100 h-20 bg-gray-50 shadow-md rounded-md flex-col items-center mb-3">
-                <Search onTextSubmit={onTextSubmit}/>
-                {error && <h4 className="text-base mt-3 text-red-300">Please make sure that you typed the correct city name</h4> }
-                <Header name={result}/>
-            </div>
-            <Description result={result}/>
+  return (
+      <Suspense fallback={<h1>Loading...</h1>}>
+      <div className="flex justify-around w-screen h-screen flex-col">
+        <div className="mt-4 flex justify-center w-100 h-20 bg-gray-50 shadow-md rounded-md flex-col items-center mb-3">
+          <Search onTextSubmit={onTextSubmit} />
+          {error && (
+              <h4 className="text-base mt-3 text-red-300">
+              Please make sure that you typed the correct city name
+            </h4>
+          )}
+          <Header name={result} />
         </div>
-    )
-}
+        <Description result={result} />
+      </div>
+          </Suspense>
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Spinner from 'react-spinner-material';
 
 const Header = (props) => {
     const name = props.name;  
@@ -8,7 +7,7 @@ const Header = (props) => {
         <div>
             <h1 className="mt-2 text-md font-bold font-sans tracking-wide">Location: <span className="text-blue-400">{name.name}</span> <span className="text-lg text-red-300 uppercase">{name.sys.country}</span></h1>
         </div>
-    ) : <Spinner />
+    ) : <h1>Loading ... </h1> 
 }
 
 export default Header
